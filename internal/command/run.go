95a2da0f9eb07f4453e88adfe31b72ba44c28422
Azuyamat <blaneyderek@gmail.com>
2025-11-18 00:30:22 -0500

    feat: Make task name optional for run command

--- internal/command/run.go
+++ internal/command/run.go
@@ -8,7 +8,7 @@ import (
 
 var runCommand = gear.NewExecutableCommand("run", "Run a specified task").
 	Args(
-		gear.NewStringArg("task", "Name of the task to run")).
+		gear.NewStringArg("task", "Name of the task to run").AsOptional()).
 	Handler(runHandler)
 
 func init() {
@@ -21,6 +21,9 @@ func runHandler(ctx *gear.Context, args gear.ValidatedArgs) error {
 		return err
 	}
 	taskName := args.String("task")
+	if taskName == "" {
+		taskName = config.DefaultTask
+	}
 	runner := runner.NewRunner(config)
 	return runner.RunTask(taskName)
 }
